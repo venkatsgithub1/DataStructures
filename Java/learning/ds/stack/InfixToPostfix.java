9e65f3321471f1181a4ec729abc0e5c3e88787b0
venkatsgithub1 <venkatsgithub@gmail.com>
2018-08-30 23:28:18 +0530

    Fixed formatting

--- Java/learning/ds/stack/InfixToPostfix.java
+++ Java/learning/ds/stack/InfixToPostfix.java
@@ -3,52 +3,50 @@ package learning.ds.stack;
 import java.util.Stack;
 
 public class InfixToPostfix {
-	
-	static int precedence(char ch)
-    {
-        switch (ch)
-        {
-        case '+':
-        case '-':
-            return 1;
-      
-        case '*':
-        case '/':
-            return 2;
-      
-        case '^':
-            return 3;
-        }
-        return -1;
-    }
+
+	static int precedence(char ch) {
+		switch (ch) {
+		case '+':
+		case '-':
+			return 1;
+
+		case '*':
+		case '/':
+			return 2;
+
+		case '^':
+			return 3;
+		}
+		return -1;
+	}
 
 	public static void main(String[] args) {
-		String str="a+b*(c^d-e)^(f+g*h)-i";
+		String str = "a+b*(c^d-e)^(f+g*h)-i";
 		String result = "";
 		Stack<Character> stack = new Stack<>();
-		for(int i=0;i<str.length();i++) {
+		for (int i = 0; i < str.length(); i++) {
 			Character ch = str.charAt(i);
-			if(Character.isLetterOrDigit(ch)) {
+			if (Character.isLetterOrDigit(ch)) {
 				result += ch;
-			} else if(ch == '(') {
+			} else if (ch == '(') {
 				stack.push(ch);
-			} else if(ch==')') {
-				while(!stack.isEmpty() && stack.peek()!='(') {
-					result+=stack.pop();
+			} else if (ch == ')') {
+				while (!stack.isEmpty() && stack.peek() != '(') {
+					result += stack.pop();
 				}
 				stack.pop();
 			} else {
-				while(!stack.isEmpty() && precedence(ch)<=precedence(stack.peek())) {
-					result+=stack.pop();
+				while (!stack.isEmpty() && precedence(ch) <= precedence(stack.peek())) {
+					result += stack.pop();
 				}
 				stack.push(ch);
 			}
 		}
-		
-		while(!stack.isEmpty()) {
-			result+=stack.pop();
+
+		while (!stack.isEmpty()) {
+			result += stack.pop();
 		}
-		
+
 		System.out.println(result);
 	}
 }
