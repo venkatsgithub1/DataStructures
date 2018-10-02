cf2f2d1ec6bca200dff749c68ef4eaaa1a01f6a2
venkatsgithub1 <venkatsgithub@gmail.com>
2018-10-02 12:08:41 +0530

    Create max_heap_implementation.py
    Max heap implementation in python

---
+++ python/minheap/interactivePython/max_heap_implementation.py
@@ -0,0 +1,83 @@
+class MaxHeapError(Exception):
+    pass
+
+
+class Max_Heap():
+
+    def __init__(self):
+        self.heap_list = [0]
+        self.current_size = 0
+
+    def size(self):
+        return self.current_size
+
+    def __len__(self):
+        return self.size()
+
+    def __repr__(self):
+        return str(self.heap_list)
+
+    def is_empty(self):
+        return self.size() == 0
+
+    def perc_up(self, i):
+        while i // 2 > 0:
+            if self.heap_list[i] > self.heap_list[i // 2]:
+                self.heap_list[i //
+                               2], self.heap_list[i] = self.heap_list[i], self.heap_list[i // 2]
+            i //= 2
+
+    def insert(self, data):
+        self.heap_list.append(data)
+        self.current_size += 1
+        self.perc_up(self.size())
+
+    def max_child(self, node):
+        if node * 2 + 1 > self.size():
+            return node * 2
+        if self.heap_list[node * 2] > self.heap_list[node * 2 + 1]:
+            return node * 2
+        return node * 2 + 1
+
+    def perc_down(self, current):
+        node = current * 2
+        while node <= self.size():
+            # get max_child
+            max_child = self.max_child(node)
+            if self.heap_list[node] < self.heap_list[max_child]:
+                self.heap_list[node], self.heap_list[max_child]
+            node = max_child
+
+    def delete_maximum(self):
+        if self.size() == 0:
+            raise MaxHeapError("Heap is empty")
+        self.heap_list[0] = self.heap_list[self.size()]
+        self.perc_down(1)
+        self.current_size -= 1
+
+
+if __name__ == "__main__":
+    heap = Max_Heap()
+    heap.insert(10)
+    print(heap)
+    heap.insert(20)
+    print(heap)
+    heap.insert(30)
+    print(heap)
+    heap.insert(50)
+    print(heap)
+    heap.insert(40)
+    print(heap)
+    heap.insert(15)
+    print(heap)
+
+
+"""
+output:
+[0, 10]
+[0, 20, 10]
+[0, 30, 10, 20]
+[0, 50, 30, 20, 10]
+[0, 50, 40, 20, 10, 30]
+[0, 50, 40, 20, 10, 30, 15]
+"""
